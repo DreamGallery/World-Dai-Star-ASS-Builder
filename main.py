96b877f77dadee31ce96cd90999bcbd44c21d93c
DreamGallery <2511462508@qq.com>
2023-08-04 17:16:23 +0800

    add mask style

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ config.read(os.path.join(BASE_PATH ,'config.ini'), encoding="utf-8")
 ASS_PATH = config.get("File PATH", "ASS_PATH")
 video_file_name = config.get("Info", "video_file_name")
 ass_file_name = config.get("Info", "ass_file_name")
+Mask_text = config.get("Mask", "Mask_text")
 
 def to_time(clip_time: float) -> str:
     H = clip_time // 3600
@@ -21,6 +22,8 @@ def to_time(clip_time: float) -> str:
 stream = frame_stream()
 start_time_list = stream.to_frame(video_file_name)
 content = script_info + "\n" + garbage + "\n" + styles + "\n" + events
+Mask_event = ass_events(layer=0, start = "0:00:00.00", end = "0:00:05.00", style = "Default", name = "遮罩示例", text = Mask_text)
+content = content + Mask_event.echo_dialogue() + "\n"
 for index in range(len(start_time_list)):
     if (index + 1) == len(start_time_list):
         start_time = to_time(start_time_list[index])
